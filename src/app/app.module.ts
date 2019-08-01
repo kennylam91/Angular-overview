747b9f2757314945fdf95ef006779ffea1c1f7de
kennylam91 <phamvanlam1991@gmail.com>
2019-08-01 13:44:43 +0700

    start component calculator

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppComponent } from './app.component';
 import { ChangeFontSizeComponent } from './change-font-size/change-font-size.component';
 import {FormsModule} from '@angular/forms';
 import { PetComponent } from './pet/pet.component';
+import { CalculatorComponent } from './calculator/calculator.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ChangeFontSizeComponent,
-    PetComponent
+    PetComponent,
+    CalculatorComponent
   ],
   imports: [
     BrowserModule,
