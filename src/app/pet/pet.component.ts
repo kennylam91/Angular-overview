1e8f3a28341ce9111415c2565994ce496175da10
kennylam91 <phamvanlam1991@gmail.com>
2019-08-01 12:24:02 +0700

    done pet component

--- src/app/pet/pet.component.ts
+++ src/app/pet/pet.component.ts
@@ -7,25 +7,8 @@ import {Component, OnInit} from '@angular/core';
 })
 export class PetComponent implements OnInit {
 
-  private _petName = 'pupie';
-  private _petImage = 'http://yourdost-blog-images.s3-ap-southeast-1.amazonaws.com/wp-content/uploads/2016/01/03165939/Dogs.jpg';
-
-
-  get petName(): string {
-    return this._petName;
-  }
-
-  set petName(value: string) {
-    this._petName = value;
-  }
-
-  get petImage(): string {
-    return this._petImage;
-  }
-
-  set petImage(value: string) {
-    this._petImage = value;
-  }
+  petName = 'pupie';
+  petImage = 'http://yourdost-blog-images.s3-ap-southeast-1.amazonaws.com/wp-content/uploads/2016/01/03165939/Dogs.jpg';
 
   constructor() {
   }
